93ba5caf4a28244cf18ca41541e84eeb2d012c9b
mohadese <mohiw.1380@gmail.com>
2026-09-05 23:47:55 +0330

    feat: test postgres

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import sequelize from "./config/database";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error : any) => {
+    console.error("Database connection failed:", error);
+  });
 
 app.get("/", (req, res) => {
   res.json({
